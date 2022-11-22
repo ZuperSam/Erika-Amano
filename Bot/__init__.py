2a84e2111aedb64fa5920bcb4f11b9ea16da050c
ViaShan <118714573+ViaShan@users.noreply.github.com>
2022-11-22 22:44:01 +0530

    Update __init__.py
    Signed-off-by: ViaShan <118714573+ViaShan@users.noreply.github.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -11,13 +11,13 @@ LOG.setLevel(level=logging.INFO)
 #<-----------Variables-------------->
 LOG.info('❤️ Checking Bot Variables.....')
 TRIGGERS = os.environ.get("TRIGGERS", "/ !").split(" ")
-BOT_TOKEN = os.environ.get('BOT_TOKEN', 'your bot token') #BOT Token Add
-API_ID = int(os.environ.get('API_ID', 456789)) #Telgram Api id
-APP_HASH = os.environ.get('APP_HASH', 'your api id')# Telgram App hash  
-OWNER_ID = int(os.environ.get('OWNER_ID', 953362604))
-MONGO_DB = os.environ.get("MONGO_DB", 'your mongodb') #MONGO DB FOR ANIME DATA
-FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -100456789013)
-BOT_NAME = os.environ.get('BOT_NAME', 'Soheru')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '5562438875:AAHKfFy6PdRkrEBqlt6w2mqghtlOKB19kSo') #BOT Token Add
+API_ID = int(os.environ.get('API_ID', 8978848)) #Telgram Api id
+APP_HASH = os.environ.get('APP_HASH', '24ce3cff2d32cf529df1c0018e28d6cf')# Telgram App hash  
+OWNER_ID = int(os.environ.get('OWNER_ID', 1995886602))
+MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://Botlover:Botlover@cluster0.e1ymmrm.mongodb.net/?retryWrites=true&w=majority') #MONGO DB FOR ANIME DATA
+FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1001810558901)
+BOT_NAME = os.environ.get('BOT_NAME', 'Zub0Compress1_bot')
 #<---------------Connecting-------------->
 if BOT_TOKEN is not None:
     try:
